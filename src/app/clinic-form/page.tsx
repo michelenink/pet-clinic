65a6f6395dfbc1c158a00c001bb9e181bbfc0fe3
michelenink <michelenink@gmail.com>
2025-05-30 09:14:42 -0300

    feat: add clinic creation form and dialog component

---
+++ src/app/clinic-form/page.tsx
@@ -0,0 +1,32 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import ClinicForm from "./components/form";
+
+const ClinicFormPage = () => {
+  return (
+    <div>
+      <Dialog open={true}>
+        <form>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Adicionar clínica</DialogTitle>
+              <DialogDescription>
+                Adicione uma clínica para que você possa gerenciar seus
+                pacientes
+              </DialogDescription>
+            </DialogHeader>
+            <ClinicForm />
+          </DialogContent>
+        </form>
+      </Dialog>
+    </div>
+  );
+};
+
+export default ClinicFormPage;
